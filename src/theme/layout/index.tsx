4bf0c01ab07c24d8235b91f0a96124944c5975a4
Khayo99 <khayolima2001@gmail.com>
2025-08-03 19:57:09 -0300

    feat: implement ScrollToTop component; update layout and router configuration; add 404 page and Vercel configuration

--- src/theme/layout/index.tsx
+++ src/theme/layout/index.tsx
@@ -1,12 +1,14 @@
 import S from './styles.module.css';
 
 import { Navbar } from '@/components/Navbar';
+import { ScrollToTop } from '@/components/ScrollToTop';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Layout: React.FC = () => {
   return (
     <div className={S.container}>
+      <ScrollToTop />
       <div className={S.topbar}>
         <Navbar />
       </div>
